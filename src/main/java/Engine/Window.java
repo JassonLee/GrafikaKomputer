ad210747228bc8baba96e7f5fa2a44ddcd7b74ce
LAPTOP-6127H8FA\Jasson <tjkl738@gmail.com>
2023-02-20 14:19:34 +0700

    test

--- src/main/java/Engine/Window.java
+++ src/main/java/Engine/Window.java
@@ -12,6 +12,7 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
+
 public class Window {
 
     private long window;
